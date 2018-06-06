808545c6354705a9f8276f479bcddd0d6bb2a425
jaehosung <jaehossung@gmail.com>
2018-06-06 22:03:51 +0900

    week1_01 Add comments

--- week1/code1_LinearRegressionForClassData.py
+++ week1/code1_LinearRegressionForClassData.py
@@ -1,6 +1,8 @@
 from scipy.stats import linregress
 import numpy as np
 import matplotlib
+
+#Anti-Grain Geometry (AGG) is an Open Source, free of charge graphic library, written in industrially standard C++.
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import elice_utils
@@ -15,18 +17,22 @@ def main():
         * social_activity
     '''
     x_col = 'social_activity' # 여기에 있는 파라미터를 바꾸면서 실행해보세요
-    
+	# Data from the colum of the files 
     (X, Y) = read_data(x_col)
+	# Do linear regression and return the b0 and b1
     (slope, intercept) = do_linear_regression(X, Y)  # linear regression을 하는 함수
-    draw_chart(X, Y, slope, intercept, x_col) # 그래프를 그리는 함수
 
+	# Drawing the chart using X,Y ,slope, intercept, x_col
+    draw_chart(X, Y, slope, intercept, x_col) 
+	
 def read_data(col_name):
     student_data = []
-    
+
     with open('students.txt') as fp:
         columns = fp.readline().strip().split(' ')
         for line in fp:
             line_data = line.strip().split(' ')
+			# make the string data to float data in a line
             line_data_numeric = [float(x) for x in line_data]
             student_data.append(line_data_numeric)
     
